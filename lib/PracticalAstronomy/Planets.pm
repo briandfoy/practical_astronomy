696a0711320acda8ff76fa4189ebfefb605c8337
brian d foy <brian.d.foy@gmail.com>
2020-11-21 13:43:52 -0500

    Start accessing planet names

---
+++ lib/PracticalAstronomy/Planets.pm
@@ -0,0 +1,39 @@
+package PracticalAstronomy::PlanetData;
+use v5.26;
+use experimental qw(signatures);
+
+use Mojo::File;
+my $MojoFile = Mojo::File->with_roles(qw(
+	Mojo::File::Role::SlurpJSON
+	Mojo::File::Role::SpurtJSON
+	));
+use Mojo::Util qw(dumper);
+
+sub new ( $class, $data ) { bless $data, $class }
+
+sub new_from_file( $class, $file ) {
+	$class->new( $MojoFile->new( $file )->slurp_json );
+	}
+
+sub _meta ( $self ) { $self->{meta} }
+sub _data ( $self ) { $self->{data} }
+
+sub planet_name_key ( $self ) { 'P' }
+sub names ( $self ) {
+	state $k = $self->planet_name_key;
+	map { $_->{$k} } $self->_data->@*;
+	}
+
+sub _data_for ( $self, $name ) {
+	state $k = $self->planet_name_key;
+	my $planet = grep { fc($_->{$k}) eq fc($name) } $self->_data->@*;
+	}
+
+sub data_for ( $self, $name ) {
+	state $k = $self->planet_name_key;
+	my $planet = $self->_data_for( $name );
+	$planet->{date} = $self->_meta->{epoch};
+	$planet;
+	}
+
+1;
